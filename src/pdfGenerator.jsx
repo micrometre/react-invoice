344ee161d5366e3781156c398b7898330fa5a4b3
Henok Wehibe <henokwehibe@gmail.com>
2025-09-12 22:31:21 +0100

    updating pdf

--- src/pdfGenerator.jsx
+++ src/pdfGenerator.jsx
@@ -202,11 +202,11 @@ const PdfGenerator = () => {
         valign: 'middle',
       },
       columnStyles: {
-        0: { cellWidth: 20, halign: 'center' },
-        1: { cellWidth: 85, halign: 'left' },
-        2: { cellWidth: 25, halign: 'center' },
-        3: { cellWidth: 30, halign: 'right' },
-        4: { cellWidth: 35, halign: 'right' },
+        0: { cellWidth: 15, halign: 'center' },
+        1: { cellWidth: 75, halign: 'left' },
+        2: { cellWidth: 20, halign: 'center' },
+        3: { cellWidth: 25, halign: 'right' },
+        4: { cellWidth: 30, halign: 'right' },
       },
       alternateRowStyles: { 
         fillColor: [248, 249, 250] 
